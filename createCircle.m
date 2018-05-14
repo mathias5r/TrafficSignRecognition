c321a205566e4753e5b852da21fdb06dd8086dff
Mathias Silva da Rosa <mathiassilva4@gmail.com>
2018-05-13 21:20:47 -0300

    Removing old files

--- createCircle.m
+++
@@ -1,9 +0,0 @@
-function [image] = createCircle(s,x,y,r)
-
-[columnsInImage rowsInImage] = meshgrid(1:s(2), 1:s(1));
-
-image = (rowsInImage - y).^2 ...
-    + (columnsInImage - x).^2 <= r.^2;
-image = imcomplement(image);
-
-end
